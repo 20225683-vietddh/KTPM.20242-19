676eeb3bad074cd3e2ed7530864c696f5f7fec75
Din19062004 <haivietb9@gmail.com>
2025-06-04 11:19:36 +0700

    Revert "Merge pull request #8 from 20225683-vietddh/feature/manage-campaign-fee"
    This reverts commit 3d48c4782c5ad853a30d8ded4c462e060f835d94, reversing
    changes made to f5926836a4d23924fc90713bc4b63eb584a3e4bb.

--- 03_Development/blue_moon/src/utils/Configs.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Configs {
-	public static final String LOGIN_PAGE_PATH = "/views/login/LoginPage.fxml";
-	public static final String LOGO_PATH = "/assets/images/logo-2.png";
-	public static final String ACCOUNTANT_HOME_PAGE_PATH = "/views/homepage/AccountantHomePage.fxml";
-	public static final String LEADER_HOME_PAGE_PATH = "/views/homepage/LeaderHomePage.fxml";
-}
